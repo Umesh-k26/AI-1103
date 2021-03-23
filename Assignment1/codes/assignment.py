afae487ce78fb5169fccd0c2ab70866d74be71c5
Umesh-k26 <80587349+Umesh-k26@users.noreply.github.com>
2021-03-23 20:12:18 +0530

    Update assignment.py

--- Assignment1/codes/assignment.py
+++ Assignment1/codes/assignment.py
@@ -1,48 +1,58 @@
-import random as rd
+import numpy as np
+from scipy.stats import binom
+
+## No. of simulations 
 simulation_length = 10000
 
-# random variable X = {0, 1, 2} count for no. of boys
-# 0 corresponds to (girl, girl)
-# 1 corresponds to (boy, girl) or (girl, boy)
-# 2 corresponds to (boy, boy)
+## No. of Children
+n = 2
+
+## Probability of child being boy 
+p = 0.5
+
+## Binomial Distribution
+data_binom = binom.rvs(n, p,size = simulation_length)
 
 
-X_0_count = X_1_count = X_2_count = 0
+## Counting number of boys in each simulation
+count_0 = count_1 = count_2 = 0
 
-sample_space = ["GG", "BG", "GB", "BB"]
 for i in range(simulation_length):
-    temp = rd.randint(0, 3)
+    if data_binom[i] == 0:
+        count_0+=1
+    elif data_binom[i] == 1:
+        count_1+=1
+    elif data_binom[i] == 2:
+        count_2+=1
 
-    #conditions in "if ladder" are w.r.t. to indices in sample_space list.
-    if(temp == 0):
-        X_0_count += 1
-    elif(temp == 1 or temp == 2):
-        X_1_count += 1
-    else:
-        X_2_count += 1
-
-X_0_prob = X_0_count/simulation_length
-X_1_prob = X_1_count/simulation_length
-X_2_prob = X_2_count/simulation_length
+## Simuation probabilities of random variable X = {0, 1, 2} where X is no. of boys  
+sim_prob_0 = count_0/simulation_length
+sim_prob_1 = count_1/simulation_length
+sim_prob_2 = count_2/simulation_length
 
 
-# To find the probablity of two boys given atleast one of them is a boy
-# By Bayes Theorem P(X=2 | X >= 1) = P(X=2) / P(X >= 1)
-#  P(X >= 1) = P(X=1) + P(X=2)
+### Pr(X = 2 | X>= 1) = Pr(X = 2)/(Pr(X = 1) + Pr(X = 2))
+prob_sim = sim_prob_2/(sim_prob_1 + sim_prob_2)
 
-X_1_or_2_prob = X_1_prob + X_2_prob
 
-simulation_result = X_2_prob / X_1_or_2_prob
+##### Calculating probablilites theoritically #####
+k = np.arange(3)
 
-print("Theoritical probabilities :")
-print("P(X = 0) = 0.25")
-print("P(X = 1) = 0.50")
-print("P(X = 2) = 0.25")
-print("P( X = 2 | X >= 1) = 0.33")
+
+## Theoritical probabilities of random variable X = {0, 1, 2} where X is no. of boys using binom.pmf
+data_theo = binom.pmf(k, n, p)
+theo_prob_0 = data_theo[0]
+theo_prob_1 = data_theo[1]
+theo_prob_2 = data_theo[2]
+
+### Pr(X = 2 | X>= 1) = Pr(X = 2)/(Pr(X = 1) + Pr(X = 2))
+prob_theo = theo_prob_2/(theo_prob_1 + theo_prob_2)
+
+
+##### Printing results #####
+print("Probability using simulation : ")
+print("P( X = 2 | X >= 1) = {0:.2f}".format(prob_sim))
 print()
 
-print("Probabilites using simulation : ")
-print("P(X = 0) = {0:.2f}".format(X_0_prob))
-print("P(X = 1) = {0:.2f}".format(X_1_prob))
-print("P(X = 2) = {0:.2f}".format(X_2_prob))
-print("P( X = 2 | X >= 1) = {0:.2f}".format(simulation_result))
+print("Theoritical probability : ")
+print("P( X = 2 | X >= 1) = {0:.2f}".format(prob_theo))
